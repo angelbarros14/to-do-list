815157f6e5d7a75d3cef33ae8546a5b186513e02
angelbarros14 <social.angelbarros@gmail.com>
2025-09-28 00:07:17 +0800

    add task list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
 
+let nextId = 0
+
 const Header = ({text}) => {
   return (
     <h1>{text}</h1>
+  )
+}
+
+const Title = ({text}) => {
+  return (
+    <h4>{text}</h4>
   )
 }
 
@@ -21,15 +29,21 @@ const Input = ({input, onChange}) => {
 const App = () => {
   const [tasks, setTasks] = useState([])
   const [input, setInput] = useState('')
+  // add state for completed tasks
+  const [complete, setComplete] = useState([])
 
   console.log(tasks)
+  
   const addTask = () => {
-    setTasks([...tasks, {input: input}])
+    setTasks([...tasks, {text: input, id: nextId++}])
     console.log('clicked')
     setInput('')
   }
 
-  // const addTask = () => 
+  const completeTask = () => {
+    setComplete([...complete, {text: tasks.text, id: tasks.id}])
+  }
+
   // const completeTask = () => 
   // const editTask = () => 
   // const removeTask = () =>
@@ -37,9 +51,13 @@ const App = () => {
   return (
     <div>
     <Header text='New Task'/>
+    <Title text='TITLE'/>
     {/* add the input to the tasks array */}
     <Button onClick={addTask} text='+' />
     <Input input={input} onChange={(e) => setInput(e.target.value)}/>
+    <Title text='TASKS'/>
+    <ul>{tasks.map(task => (<li key={task.id}>{task.text}</li>))}</ul>
+
     
     </div>
   )
